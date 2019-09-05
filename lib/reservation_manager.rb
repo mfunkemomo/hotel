680a740f18019eb503a53b1b21c1c6052ffa3eb6
Monick <monick.keo@gmail.com>
2019-09-05 00:37:41 -0700

    added rooms method, reservation_list method, and book_room method to reservation_manager class. Have not written tests yet

--- lib/reservation_manager.rb
+++ lib/reservation_manager.rb
@@ -22,35 +22,41 @@ module HotelBookings
       end
       return room_list
     end 
-
-    #auto assigns a rooom number when making a reservation instance
-
-    #room status (availability) for a certain day 
-    def room_status(date)
-      #go through room_list
-      #for each room, look for the date 
-      #check if the status for that date 
-    end 
   
-    #list of current reservations for a certain day
     def reservation_list(date)
-      #for each reservation instance, check for the given date
-      #if the date
-      #return list of reservations
-      
+      #NEED TO GO THROUGH RESERVATION INSTANCES 
+      unavailable_rooms = []
+      rooms.each { |room, details| 
+        details.each do |dates|
+          if dates != date
+            if unavailable_rooms.include?(room) == false
+              unavailable_rooms.push(room)
+            end 
+          end 
+        end 
+        }
+      return unavailable_rooms
     end
     
-    #book a room 
     def book_room
-      # find the first room that is available 
-      # if room in room list is available make room_no equal that room
-      # available_room = room_list.find{|room| room.status == :available}
-      # room_no = available_room.room_no
+      @dates = Reservation_Dates.new(checkin:@checkin, checkout:@checkout)
+      days = []
+      day = @dates.checkin
+      @dates.total_nights.times do 
+        days.push(day)
+        day = day + 1
+      end 
 
-      # # make a reservation instance
-      new_reservation = Reservation.new(customer_name:@customer_name, checkin: @checkin, checkout: @checkout, room_no)
-      # #add reservation instance to reservation list - WHERE IS RESERVATION LIST
-      # reservation_list.push(new_reservation)
+      days.each do |day|
+        check = reservation_list(day)
+        i = 0
+        for i..check.length
+          if rooms[i] != check[i]
+            available_room = room[i]
+          end 
+          new_reservation = Reservation.new(customer_name:@customer_name, checkin: @checkin, checkout: @checkout, room_no: available_room)
+        end 
+      end 
     end 
   end 
 end 
